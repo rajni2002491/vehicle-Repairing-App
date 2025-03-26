2830918fceb4553fbba8f52aa17894c6ef38cd03
Rajnikant <rajnikantb2002@gmail.com>
2025-03-26 20:18:55 +0530

    adding animated SplashScreen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,63 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/logins/login_screen.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:lottie/lottie.dart'; // Import the lottie package
+import 'logins/login_screen.dart'; // Import the LoginScreen
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    // Auto navigate after 5 seconds
-    Future.delayed(Duration(seconds: 3), () {
-      Navigator.push(
-        // ignore: use_build_context_synchronously
-        context,
-        MaterialPageRoute(builder: (context) => LoginScreen(userName: 'defaultUser')),
-      );
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white, // Light background
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/Repair.jpg', // Ensure this image is added to assets folder
-              height: 250,
-            ),
-            Text(
-              "QuickFix - Vehicle Repair",
-              style: TextStyle(
-                fontSize: 26,
-                fontWeight: FontWeight.bold,
-                color: Colors.black, // Better visibility
-              ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 10),
-            Text(
-              "Fast, Reliable, and Nearby Repair Shops",
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.black87,
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 30),
-          ],
-        ),
+    return AnimatedSplashScreen(
+      splash: Center(
+       child: Lottie.asset(
+         'assets/animation/Animation - 1742998118287.json',
+         height: 800,
+         width: 650,
+         fit: BoxFit.cover,
+         // Correct path to the animation
+       ),
       ),
+      nextScreen: const LoginScreen(userName: '',), // Navigate to LoginScreen
+      splashTransition: SplashTransition.fadeTransition,
+      duration: 3500,
+      backgroundColor: Colors.white, // Only one backgroundColor
     );
   }
 }
